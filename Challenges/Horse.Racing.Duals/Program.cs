c66e5bef1c7de5d9056ca037866ea64dae550b75
Juan Sebastian Santa <jsantahernandez@gmail.com>
2023-01-06 11:10:18 -0500

    Add horse-racing-duals challenge

---
+++ Challenges/Horse.Racing.Duals/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Power.Of.Thor.Episode._1
+{
+  internal static class Program
+  {
+    static void Main(string[] args)
+    {
+      int N = int.Parse(Console.ReadLine());
+      int[] positions = new int[N];
+      int minDiff = int.MaxValue;
+      for (int i = 0; i < N; i++)
+      {
+        int pi = int.Parse(Console.ReadLine());
+        positions[i] = pi;
+      }
+      positions = positions.OrderBy(x => x).ToArray();
+      for (int i = 0; i < positions.Length - 1; i++)
+      {
+        int diff = Math.Abs(positions[i] - positions[i + 1]);
+        if (diff < minDiff)
+        {
+          minDiff = diff;
+        }
+      }
+
+      Console.WriteLine(minDiff);
+    }
+  }
+}
---
+++ Challenges/Horse.Racing.Duals/README.md
@@ -0,0 +1,7 @@
+# Horse-racing Duals
+
+## 🚀 The Goal
+
+Casablanca’s hippodrome is organizing a new type of horse racing: duals. During a dual, only two horses will participate in the race. In order for the race to be interesting, it is necessary to try to select two horses with similar strength.
+
+Write a program which, using a given number of strengths, identifies the two closest strengths and shows their difference with an integer (≥ 0).
